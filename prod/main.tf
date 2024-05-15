02d6a86ec8a94bf68c8aaf2df5d9c42d9f4305ff
KeekaYagnash <yagnash.keeka@disraptor.co.za>
2024-05-15 15:26:02 +0200

    update

--- prod/main.tf
+++ prod/main.tf
@@ -99,7 +99,7 @@ resource "aws_codebuild_project" "test" {
   service_role  = aws_iam_role.codebuild_role.arn
 
   artifacts {
-    type = "No_ARTIFACTS"
+    type = "NO_ARTIFACTS"
   }
   cache {
     type     = "S3"
@@ -127,7 +127,7 @@ resource "aws_codebuild_project" "test" {
     }
   }
   source {
-    type            = "CODECOMMIT "
+    type            = "CODECOMMIT"
     location        = "https://git-codecommit.us-east-1.amazonaws.com/v1/repos/testrepo"
     git_clone_depth = 1
 
