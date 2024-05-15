692c180030fed0b0d7d6f47c24ca49c5cca8e3eb
KeekaYagnash <yagnash.keeka@disraptor.co.za>
2024-05-15 15:30:55 +0200

    update

--- prod/main.tf
+++ prod/main.tf
@@ -16,10 +16,10 @@ resource "aws_s3_bucket" "cb_bucket" {
   bucket = "test-terraform-codebuild-drpt-bucket"
 }
 
-resource "aws_s3_bucket_acl" "s3_acl" {
-  bucket = aws_s3_bucket.cb_bucket.id
-  acl    = "private"
-} 
+# resource "aws_s3_bucket_acl" "s3_acl" {
+#   bucket = aws_s3_bucket.cb_bucket.id
+#   acl    = "private"
+# } 
 
 data "aws_iam_policy_document" "assume_role" {
   statement {
